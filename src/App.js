a8ba6a9217f5a4593e7b3e28c3c6fddea74b2800
Cody Lobdell <thecreator85@gmail.com>
2023-03-01 19:01:38 -0800

    last push/pull before lab 03

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
-import React from 'react';
-import Header from './Header';
-import Main from './Main';
-import data from './data.json';
-import Footer from './Footer';
+import React from "react";
+import Header from "./Header";
+import Main from "./Main"
+import Footer from "./Footer";
+import data from "./data.json"
+import Modal from 'react-bootstrap/Modal'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header title="Gallery of Horns"/>
-      <Main id='My Brain is Vaporizing'/>
-      <Footer author='Cody L.'/>
-    </div>
-  );
-}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      image_url: '',
+      title: '',
+      description:'',
+      isShown: false
+    }
+  }
+
+  //  Functions for Modal
+
+  openModal = (title,url,description) => {
+    this.setState({
+      isShown: true,
+      image_url: url,
+      title: title,
+      description: description,
+    })
+  }
+
+  closeModal = () => {
+    this.setState({
+      isShown: false
+    })
+  }
+
+  render(){
+    return (
+    <>
+    <Header/>
+    <Main 
+    data = {data}
+    openModal = {this.openModal}
+    />
+    <Footer />
+      <Modal
+       show={this.state.isShown} onHide={this.closeModal} size="lg"
+       >
+        <Modal.Header closeButton>
+          <Modal.Title>{this.state.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+         <img src={this.state.image_url} alt={this.state.title}/>
+         <p>{this.state.description}</p>
+         
+        </Modal.Body>
+
+        </Modal>
+    </>
+    );
+  }
+};
 
 export default App;
