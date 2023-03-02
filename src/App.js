9c339ddacb3227341471f896e3d2daaf3fa6b995
Cody Lobdell <thecreator85@gmail.com>
2023-03-02 13:53:09 -0800

    finished for m. Couldn't get it to select specific return for form though

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main"
 import Footer from "./Footer";
 import data from "./data.json"
 import Modal from 'react-bootstrap/Modal'
+import FormSelect from "./form"
 import './App.css';
 
 
@@ -35,10 +36,61 @@ class App extends React.Component {
     })
   }
 
+  filterHorns = (e) => {
+    e.preventDefault();
+    if (this.state.selectHorns === "4") {
+      let newData = data.filter(i =>
+        i.horns >= 4
+      );
+      this.setState({
+        selectData: newData
+      })
+    } else if (this.state.selectHorns === "1") {
+      let newData = data.filter(i =>
+        i.horns === 1
+      );
+      this.setState({
+        selectData: newData
+      })
+    } else if (this.state.selectHorns === "2") {
+      let newData = data.filter(i =>
+        i.horns === 2
+      );
+      this.setState({
+        selectData: newData
+      })
+    } else if (this.state.selectHorns === "3") {
+      let newData = data.filter(i =>
+        i.horns === 3
+      );
+      this.setState({
+        selectData: newData
+      })
+
+    } else if (this.state.selectHorns === "all") {
+      this.setState({
+        selectData: data
+      })
+
+    }
+  }
+
+  select = (e) => {
+    this.setState({
+      selectHorns: e.target.value
+    })
+  }
+
   render() {
     return (
       <>
         <Header />
+
+        <FormSelect
+          filterHorns={this.filterHorns}
+          select={this.select}
+
+        />
         <Main
           data={data}
           openModal={this.openModal}
